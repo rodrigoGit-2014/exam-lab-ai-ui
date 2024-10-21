dfdf339d7549164adddd5c2a5f99f6f73aa6e0c7
paravena <adla_paravena@contractor.indeed.com>
2024-10-21 16:33:38 -0300

    adding table results

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
 import FileUploadComponent from "./FileUploadComponent.tsx";
+import LabResultsTable from "./LabResultsTable.tsx";
 
 function App() {
   return (
     <>
       <FileUploadComponent />
+      <LabResultsTable rut="5344119K" />
     </>
   )
 }
---
+++ src/LabResultsTable.tsx
@@ -0,0 +1,80 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+type Glicemia = {
+    result: number;
+    unit: string;
+}
+
+type NitrogenoUreico = {
+    reference_range: string;
+}
+
+type LabResult = {
+    id: string;
+    rut: string;
+    examDate: string;
+    glicemia: Glicemia;
+    nitrogenoUreico: NitrogenoUreico
+};
+
+type Props = {
+    rut: string;
+}
+
+const LabResultsTable = ({ rut }: Props) => {
+    const [labResults, setLabResults] = useState<LabResult[]>([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(`https://exam-lab-ai-production.up.railway.app/api/laboratory/find?rut=${rut}`);
+                setLabResults(response.data.listOfExams);
+            } catch (error) {
+                console.error('Error fetching the lab results', error);
+            }
+        };
+        fetchData();
+    }, [rut]);
+
+    return (
+        <div className="max-w-4xl mx-auto mt-10">
+            <table className="min-w-full bg-white border border-gray-300">
+                <thead>
+                <tr>
+                    <th className="py-2 px-4 border-b">ID</th>
+                    <th className="py-2 px-4 border-b">RUT</th>
+                    <th className="py-2 px-4 border-b">Exam Date</th>
+                    <th className="py-2 px-4 border-b">Glicemia</th>
+                    <th className="py-2 px-4 border-b">Nitrogeno Ureico</th>
+                </tr>
+                </thead>
+                <tbody>
+                {labResults.map((result) => (
+                    <tr key={result.id}>
+                        <td className="py-2 px-4 border-b text-center">{result.id}</td>
+                        <td className="py-2 px-4 border-b text-center">{result.rut}</td>
+                        <td className="py-2 px-4 border-b text-center">{result.examDate}</td>
+                        <td className="py-2 px-4 border-b text-center">
+                            {result.glicemia ? (
+                                <span className="text-green-500">&#10003;</span>
+                            ) : (
+                                "-"
+                            )}
+                        </td>
+                        <td className="py-2 px-4 border-b text-center">
+                            {result.nitrogenoUreico ? (
+                                <span className="text-green-500">&#10003;</span>
+                            ) : (
+                                "-"
+                            )}
+                        </td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default LabResultsTable;
